f837abf09890f156cb192049b7ac8284d8432950
Marcos Lima <94881434+marcosdelima26@users.noreply.github.com>
2023-08-01 18:37:54 -0300

    Tela-login.JS
    Tela de Login, no Java Scritp

---
+++ Tela-de-login.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Login</title>
+</head>
+<body>
+    <selection class="area-login">
+        <div class="login">
+        
+        </div>  
+        
+        <form method="POST">
+            <label for="mail">Usuário: </label>
+            <input type="text" name="nome" placeholder="nome de usuário" autofocus="">
+            <label for="name">Senha:</label>
+            <input type="password" name="senha" placeholder="digite a senha">
+            <input type="submit" value="Login">
+        </form>
+        <p>Ainda não possui cadastro?<a href="#"> Criar conta </a></p>
+    </selection>
+</body>
+</html>
